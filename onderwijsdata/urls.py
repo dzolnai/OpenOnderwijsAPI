a89eaddaba24e6e3e0e899610078b5df3cb008e6
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2013-10-23 10:59:22 +0200

    change paths

--- onderwijsdata/urls.py
+++ onderwijsdata/urls.py
@@ -7,8 +7,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
 	# Examples:
 	# url(r'^$', 'onderwijsdata.views.home', name='home'),
-	url(r'^api/',      include('api.urls')),
-	url(r'^api-docs/', include('rest_framework_swagger.urls')),
+	url(r'^docs/', include('rest_framework_swagger.urls')),
+	url(r'^',      include('api.urls')),
 
 	# Uncomment the admin/doc line below to enable admin documentation:
 	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
