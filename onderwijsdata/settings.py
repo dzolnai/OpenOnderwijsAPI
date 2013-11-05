b9e09bce4bb6d6a9f3cf37fffd93e6c409cce9d4
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2013-11-05 12:10:23 +0100

    Swagger settings

--- onderwijsdata/settings.py
+++ onderwijsdata/settings.py
@@ -173,16 +173,10 @@ REST_FRAMEWORK  = {
 }
 
 SWAGGER_SETTINGS = {
-	"exclude_namespaces": [], # List URL namespaces to ignore
+	"exclude_namespaces": ['grouproles'], # List URL namespaces to ignore
 	"api_version": '0.1',  # Specify your API's version
-	"api_path": "/",  # Specify the path to your API not a root level
-	"enabled_methods": [  # Specify which methods to enable in Swagger UI
-		'get',
-		'post',
-		'put',
-		'patch',
-		'delete'
-	],
+	#"api_path": "/",  # Specify the path to your API not a root level
+	"enabled_methods": [ 'get' ],
 	"api_key": '', # An API key
 	"is_authenticated": False,  # Set to True to enforce user authentication,
 	"is_superuser": False,  # Set to True to enforce admin only access
